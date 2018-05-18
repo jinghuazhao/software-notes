d10ca63bacbaf0d98177a2bb0f8d9c9ca13607c2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-05-18 09:09:25 +0100

    MetaXcan

--- MetaXcan/V7.sh
+++ MetaXcan/V7.sh
@@ -1,10 +1,10 @@
-# 17-5-2018 JHZ
+# 18-5-2018 JHZ
 
-cd /home/jhz22/D/genetics/hakyimlab/ftp/MetaXcan/software
+cd /home/jhz22/D/genetics/hakyimlab/MetaXcan/software
 
 ./MetaXcan.py \
---model_db_path /home/jhz22/D/genetics/hakyimlab/ftp/GTEx-V7_HapMap-2017-11-29/gtex_v7_Brain_Amygdala_imputed_europeans_tw_0.5_signif.db \
---covariance /home/jhz22/D/genetics/hakyimlab/ftp/GTEx-V7_HapMap-2017-11-29/gtex_v7_Brain_Amygdala_imputed_eur_covariances.txt.gz \
+--model_db_path /home/jhz22/D/genetics/hakyimlab/PredictDB/GTEx-V7_HapMap-2017-11-29/gtex_v7_Brain_Amygdala_imputed_europeans_tw_0.5_signif.db \
+--covariance /home/jhz22/D/genetics/hakyimlab/PredictDB/GTEx-V7_HapMap-2017-11-29/gtex_v7_Brain_Amygdala_imputed_eur_covariances.txt.gz \
 --gwas_folder data/GWAS \
 --gwas_file_pattern ".*gz" \
 --snp_column SNP \
