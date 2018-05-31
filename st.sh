566353a3307bf994c0a7e657b8fbb9a73ba12a9f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-05-31 15:33:08 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 27-5-2018 JHZ
+# 31-5-2018 JHZ
 
 git add README.md
 git commit -m "README"
-for d in DEPICT JAM ldsc PASCAL MetaXcan PLINK2 PyLMM R R-packages PheWAS overall VEP st.sh
+for d in DEPICT caviar JAM ldsc PASCAL MetaXcan PLINK2 PyLMM R R-packages PheWAS overall VEP st.sh
 do
    git add $d
    git commit -m "$d"
