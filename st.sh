feced2b4522690bfac2441599880054a26c1408d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-12 13:34:56 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add README.md
 git commit -m "README"
-for d in DEPICT caviar JAM jannovar ldsc PASCAL MetaXcan PLINK2 PyLMM R R-packages PheWAS overall VEP varscan st.sh
+for d in DEPICT caviar JAM jannovar ldsc PASCAL MetaXcan PLINK2 PyLMM R R-packages PheWAS overall VEP VarScan st.sh
 do
    git add $d
    git commit -m "$d"
