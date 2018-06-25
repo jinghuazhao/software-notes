aa2e7046acb00136f853aec80118274c25d549a1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-25 16:07:38 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,7 +1,9 @@
-# 15-6-2018 JHZ
+# 25-6-2018 JHZ
 
 git add README.md
 git commit -m "README"
+git add topics.md
+git commit -m "Specific topics"
 git add NOTES.md
 git commit -m "NOTES"
 for d in DEPICT caviar GATK IGV IMPHLA02 JAM jannovar ldsc PASCAL MetaXcan PLINK2 PyLMM R R-packages PheWAS overall VEP VarScan st.sh
