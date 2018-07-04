5a35d5695023eaed7d44f4a4fa6043a0a4db40a1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-07-04 20:00:22 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 3-7-2018 JHZ
+# 4-7-2018 JHZ
 
 git add README.md
 git commit -m "README"
@@ -6,7 +6,7 @@ git add topics.md
 git commit -m "Specific topics"
 git add NOTES.md
 git commit -m "NOTES"
-for d in DEPICT bowtie2 caviar GATK IGV IMPHLA02 JAM jannovar ldetect ldsc ldpred PASCAL MetaXcan FUSION PLINK2 PyLMM R R-packages PheWAS overall VEP VarScan st.sh
+for d in DEPICT bowtie2 caviar GATK IGV IMPHLA02 JAM jannovar ldetect ldsc ldpred PASCAL MetaXcan FUSION PLINK2 PyLMM R R-packages PheWAS pindel overall VEP VarScan st.sh
 do
    git add $d
    git commit -m "$d"
