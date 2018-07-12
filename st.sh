fe3c42b791bd0899ffb527f3471040e79f307c25
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-07-12 11:52:10 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,11 +1,13 @@
-# 4-7-2018 JHZ
+# 12-7-2018 JHZ
 
 git add README.md
 git commit -m "README"
 git add topics.md
 git commit -m "Specific topics"
-git add NOTES.md
-git commit -m "NOTES"
+git add AI.md
+git commit -m "AI"
+git add seq.md
+git commit -m "sequencing analysis"
 for d in DEPICT bowtie2 caviar GATK IGV IMPHLA02 JAM jannovar ldetect ldsc ldpred PASCAL MetaXcan FUSION PLINK2 PyLMM R R-packages PheWAS pindel overall VEP VarScan st.sh
 do
    git add $d
