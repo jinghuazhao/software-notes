cce16d688e46fbbfec56a72ca8ccc34ac95feb18
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-19 21:54:35 +0000

    Deployed 63cd614 with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -29,8 +29,19 @@ function old()
   git push
 }
 
-module load python/3.7
-source ~/COVID-19/py37/bin/activate
+function setup()
+{
+  module load python/3.8
+  source ~/rds/public_databases/software/py38/bin/activate
+}
+
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+   module load ceuadmin/krb5
+fi
+
+setup
 mkdocs build
 mkdocs gh-deploy
 git add .gitignore
